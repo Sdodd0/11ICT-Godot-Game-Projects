5670772d06cb33f3a60dcfd49fa0ec0acaaa180d
Sdodd0 <seth.dodd@icloud.com>
2026-07-29 13:41:24 +1000

    New Version

--- Unnamed Game 2/main_character.gd
+++ Unnamed Game 2/main_character.gd
@@ -1,6 +1,8 @@
 extends CharacterBody3D
+
+
 @export var speed: float = 200
-@export var target = WorldManager.Current_Position
+@onready var target1 = $Grapple_Hook
 @export var mouse_sensitivity: float = 0.002
 @export var min_pitch: float = -89.0
 @export var max_pitch: float = 89.0
@@ -17,39 +19,28 @@ func _unhandled_input(event: InputEvent) -> void:
 		
 		camera.rotation_degrees.x = clamp(camera.rotation_degrees.x, min_pitch, max_pitch)
 
-func Grapple_physics_process(delta: float) -> void:
-	if target:
-		var direction: Vector3 = global_position.direction_to(target.global_position)
-		velocity = direction * speed
-	move_and_slide()
-
-# grappling script
-func Grapple_gun_ready():
-	if GrappleHook.Grapple_Hook_Ready:
-		var direction: Vector3 = global_position.direction_to(target)
-		velocity = direction * speed
-		move_and_slide()
-	if not GrappleHook.Grapple_Hook_Ready:
-		pass
-
-func Grapple_Gun_Use():
-	if Input.is_action_just_pressed("Right_Mouse_Button"):
-		Grapple_gun_ready()
-
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
-
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+# Constants
+	const SPEED = 5.0
+	const JUMP_VELOCITY = 4.5
+
+# Handle gravity
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
+# Handle grappling gun
+	if Input.is_action_just_pressed("Right_Mouse_Button"):
+		if GrappleHook.Grapple_Hook_Ready == true:
+			var grapple_direction: Vector3 = global_position.direction_to(target1)
+			velocity = grapple_direction * speed
+		if GrappleHook.Grapple_Hook_Ready == false:
+			pass
+
+# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+# Handle player movement
 	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
--- Unnamed Game 2/world_manager.gd
+++ Unnamed Game 2/world_manager.gd
@@ -1,7 +1,5 @@
 extends Node3D
 
-var Current_Position
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass
@@ -9,7 +7,7 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	var Current_Position = global_position
+	pass
 
 
 
